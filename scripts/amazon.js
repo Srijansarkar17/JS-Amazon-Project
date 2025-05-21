82a80bcb5c3643a67b94a50b1f695f409589fa59
Srijansarkar17 <srijanb463@gmail.com>
2025-05-22 03:11:17 +0530

    generated the html code through javascript
    generated the product-container code through js

---
+++ scripts/amazon.js
@@ -0,0 +1,96 @@
+//we need to save data(information about our products), so we will store all of our information data in this js file
+
+//we are creating a list of products
+//and for each product we have product name, price, rating so for that we will use object
+//so inside the list we create an object
+const products = [{ //for the first product
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating: { //rating has two things,the no of stars and the count so we created an object for rating
+        stars: 4.5,
+        count: 87
+    },
+    priceCents: 1090 //js has some problems in 0.1 or 0.9 so we store in cents(it means $10.90)
+}, { //for the second product
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating: {
+        stars: 4,
+        count: 127
+    },
+    priceCents: 2095
+}, { //for the third product
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating: {
+        stars: 4.5,
+        count: 56
+    },
+    priceCents: 799
+}];
+//NOW WE WILL BE GENERATING HTML USING THIS DATA INSTEAD OF WRITING THE HTML MANUALLY
+
+//TO GENERATE THE HTML, WE CAN LOOP THROUGH THE PRODUCTS ARRAY and FOR EACH OF THESE PRODUCTS WE ARE GONNA CREATE SOME HTML
+
+//in this forEach loop, it takes each object, stores it in the parameter called product and then runs the function
+
+let productsHTML = '';
+
+products.forEach((product) => { //basically we are looping through each product inside the array and creating html for each product
+    let html = `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"> <!--we multiplied it by 10 because the image is stored in that notation only for example if the rating is 4.5 the image is stored as 45-->
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${product.priceCents/100}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+    `;
+    //the last step is to combine all the html together and put it all on the web page
+    productsHTML+=html;
+});
+
+console.log(productsHTML);
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
