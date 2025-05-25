150aebcf13f04310e0c30fa7ce6192462005ede9
Srijansarkar17 <srijanb463@gmail.com>
2025-05-25 23:34:34 +0530

    added the checkout.js file in which i saved the data, displayed html

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,11 @@
 //Cart page functionality
-export const cart = []; //export is to let the cart variable be used outside of cart.js file
+export const cart = [{
+    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+}, {
+    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1
+}]; //export is to let the cart variable be used outside of cart.js file
 
 export function addToCart(productId) {
   //Steps to add an item in the cart
