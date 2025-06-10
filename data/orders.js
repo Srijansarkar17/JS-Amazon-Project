e6a0badbcef0b0160fa4fef7295666c888c84098
Srijansarkar17 <srijanb463@gmail.com>
2025-06-10 22:38:24 +0530

    Tried to save data and generate HTML in orders.js
    Some bugs left, need to solve it

--- data/orders.js
+++ data/orders.js
@@ -8,3 +8,58 @@ export function addOrder(order) {
 function saveToStorage() {
     localStorage.setItem('orders', JSON.stringify(orders));
 }
+console.log(orders);
+
+function renderOrdersHTML() {
+    let orderSummaryHTML = '';
+    orders.forEach((orderItem) => {
+        const orderId = orderItem.id;
+        const orderTime = orderItem.orderTime;
+        const TotalCost = orderItem.totalCostCents;
+        orderItem.products.forEach((productDetails) => {
+            const productId = productDetails.productId;
+            const deliveryTime = productDetails.
+                estimatedDeliveryTime;
+            const quantity = productDetails.quantity;
+
+
+
+            orderSummaryHTML = '';
+
+            let html = `
+            <div class="product-image-container">
+              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            </div>
+
+            <div class="product-details">
+              <div class="product-name">
+                Black and Gray Athletic Cotton Socks - 6 Pairs
+              </div>
+              <div class="product-delivery-date">
+                Arriving on: ${deliveryTime}
+              </div>
+              <div class="product-quantity">
+                Quantity: ${quantity}
+              </div>
+              <button class="buy-again-button button-primary">
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+              </button>
+            </div>
+
+            <div class="product-actions">
+              <a href="tracking.html">
+                <button class="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+        `;
+            orderSummaryHTML += html;
+        })
+        console.log(orderSummaryHTML);
+    });
+    document.querySelectorAll('.js-order-details').innerHTML = orderSummaryHTML;
+}
+renderOrdersHTML();
+
